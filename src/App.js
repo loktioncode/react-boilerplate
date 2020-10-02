330c1115b54be67576de12d297de7ae80d6b0541
loktioncode <elishabere4@gmail.com>
2020-10-02 21:49:01 +0200

    update packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import Home from './components/Home';
-import About from './components/About';
+import Home from "./components/Home";
+import About from "./components/About";
 
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
 function App() {
-
-    return (
-        <div className="app">
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Home}></Route>
-                    <Route path="/about" component={About}></Route>
-                </Switch>
-            </Layout>
-        </div>
-    );
+  return (
+    <div className="app">
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+        </Switch>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
